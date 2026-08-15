2c5722295f3b4008dc7a0629d596342759d2e6d7
BlackWood <bigxho15@gmail.com>
2026-08-15 15:12:52 +0200

    Update wsgi.py

--- project_encryption/wsgi.py
+++ project_encryption/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pro_enc.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_encrytion.settings')
 
 application = get_wsgi_application()
