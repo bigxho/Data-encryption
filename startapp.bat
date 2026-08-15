db93694d2e3fcd4e9928166a210fcd5f8638cb52
BlackWood <bigxho15@gmail.com>
2026-08-15 15:16:06 +0200

    Update startapp.bat

--- startapp.bat
+++ startapp.bat
@@ -4,11 +4,10 @@ rem @start cmd
 @cd /D %~dp0
 @ping -n 2 127.0.0.1 >null
 @cls
-rem @start "C:\Program Files\Mozilla Firefox\firefox.exe" http://127.0.0.1:8000/
+rem
 @call .\penv\Scripts\activate
 rem
 @echo Starting webapp Djanngo5
 @ping -n 3 127.0.0.1 >null
-python manage.py runserver | @start "C:\Program Files\Mozilla Firefox\firefox.exe" http://127.0.0.1:8000/
+python manage.py runserver | @start "%programfiles%\Mozilla Firefox\firefox.exe" http://127.0.0.1:8000/
 rem
-rem @start "C:\Program Files\Mozilla Firefox\firefox.exe" http://127.0.0.1:8000/
